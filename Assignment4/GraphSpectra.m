5f1536df0f19da631bf91ea9e3b2453d1ae8973d
Gabriele Morello <55058684+GGmorello@users.noreply.github.com>
2022-12-03 21:05:03 +0100

    added assignment4

---
+++ Assignment4/GraphSpectra.m
@@ -0,0 +1,45 @@
+clc;
+clear all;
+close all;
+
+k = 4;
+
+T1 = csvread('example1.dat');
+T2 = csvread('example2.dat');
+
+col1 = T1(:, 1);
+col2 = T1(:, 2);
+
+%1.
+gr = graph(col1, col2);
+adj = adjacency(gr);   
+
+figure(1)
+
+
+A = full(adj); 
+figure(2)
+spy(A,'r'); 
+
+%2.
+D = diag(sum(A,2));
+L = (D^(-0.5))*A*(D^(-0.5));
+
+%3.
+[X, eigval] = eigs(L, 100, 'SA');
+
+%4.
+Y = X./sqrt(sum(X.^2, 2));
+
+%5.
+[idx,C] = kmeans(Y, k)
+
+%6
+figure(3)
+[eigvecs, eigvals] = eig(L);
+plot(sort(eigvecs(:,2)))
+
+figure(4)
+[eigvecs, eigvals] = eig(D-A);
+plot(sort(eigvecs(:,2)))
+
