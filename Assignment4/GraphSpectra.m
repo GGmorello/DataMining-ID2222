b8bbb538b87f4cf7392f00b2797788eff99b2ae1
Gabriele Morello <55058684+GGmorello@users.noreply.github.com>
2022-12-05 20:44:57 +0100

    final commit

--- Assignment4/GraphSpectra.m
+++ Assignment4/GraphSpectra.m
@@ -2,44 +2,49 @@ clc;
 clear all;
 close all;
 
-k = 4;
+k = 2;
 
 T1 = csvread('example1.dat');
 T2 = csvread('example2.dat');
 
-col1 = T1(:, 1);
-col2 = T1(:, 2);
+col1 = T2(:, 1);
+col2 = T2(:, 2);
 
 %1.
 gr = graph(col1, col2);
-adj = adjacency(gr);   
-
-figure(1)
-plot(gr)
-
+adj = adjacency(gr);
 A = full(adj); 
-figure(2)
-spy(A,'r'); 
+spy(A,'r');
+
+figure(11);
+plot(eig(adj));
 
 %2.
-D = diag(sum(A,2));
-L = (D^(-0.5))*A*(D^(-0.5));
+D=diag(sum(A,2));           
+L = D^(-1/2)*A*D^(-1/2);
+
+L1 = D - A;
 
 %3.
-[X, eigval] = eigs(L, 100, 'SA');
+[X,~] = eigs(L,k);
 
 %4.
-Y = X./sqrt(sum(X.^2, 2));
+Y=X./sum(X.*X,2).^(1/2);
 
 %5.
-[idx,C] = kmeans(Y, k)
+idx = kmeans(Y, k);
 
 %6
-figure(3)
-[eigvecs, eigvals] = eig(L);
-plot(sort(eigvecs(:,2)))
+figure;
+hold on;
+graph_adj = graph(adj);
+h = plot(graph_adj);
+cluster_colors=hsv(k);
+for i=1:k
+    cluster_members=find(idx==i);
+    highlight(h, cluster_members , 'NodeColor', cluster_colors(i,:))
+end
 
-figure(4)
-[eigvecs, eigvals] = eig(D-A);
+figure(5)
+[eigvecs, ~] = eigs(L1, k, 'SA');
 plot(sort(eigvecs(:,2)))
-
